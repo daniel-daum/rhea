fee5e5ed0bf4a66ab0829af7651a94b9e0bdbae2
Daniel Daum <daniel@danieldaum.net>
2025-11-14 19:07:02 -0800

    Rhea 15 store form (#73)
    * feat(app): rhea-15 add views and view rendering
    
    added views, and render them to the main window
    
    * update chain form (not store)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
 import Sidebar from "./components/sidebar/sidebar/Sidebar";
+import GroceryView from "./components/views/GroceryView";
+import ReceiptView from "./components/views/RecieptView";
+import StoreView from "./components/views/StoreView";
+import ChainView from "./components/views/ChainView";
 
 function App() {
+  const [activeView, setActiveView] = useState("Store");
+
+  const renderView = () => {
+    switch (activeView) {
+      case "Chain":
+        return <ChainView />;
+      case "Store":
+        return <StoreView />;
+      case "Reciept":
+        return <ReceiptView />;
+      case "Groceries":
+        return <GroceryView />;
+      default:
+        return <ChainView />;
+    }
+  };
+
   return (
-    <>
-      <Sidebar></Sidebar>
-    </>
+    <div className="app-sidebar">
+      <Sidebar onViewChange={setActiveView}></Sidebar>
+      <div className="app-main">{renderView()}</div>
+    </div>
   );
 }
 
---
+++ frontend/src/components/views/RecieptView.tsx
@@ -0,0 +1,5 @@
+function ReceiptView() {
+  return <div>Reciept view</div>;
+}
+
+export default ReceiptView;
